b6ce66415f83961a0afddf8941bb7afc166d20cf
nessed <ali.abid44444@gmail.com>
2023-08-01 22:13:41 +0500

    run

---
+++ bubbleseffect/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div class="title">MARS</div>
+      <div class='main-area'>
+        <div class='circles'>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
